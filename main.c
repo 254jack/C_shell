d94755ea7de1cd7ac85bcb9ec0baa0bc503df3c4
254jack <taixonj@gmail.com>
2023-07-26 00:17:21 +0300

    main.c

---
+++ main.c
@@ -0,0 +1,47 @@
+#include "shell.h"
+#include <stdio.h>
+
+/**
+ * main - The main program
+ * @argc - argument count
+ * @argv - argument vector
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		while (1)
+		{
+			if (prompt() == -1)
+				break;
+		}
+	}
+	else if (argc == 2)
+	{
+		FILE *file = fopen(argv[1], "r");
+
+		if (file == NULL)
+		{
+			perror("Error opening file");
+			return (1);
+		}
+
+		char line[MAX_CMD_LENGTH];
+
+		while (fgets(line, sizeof(line), file))
+		{
+			line[strcspn(line, "\n")] = '\0';
+			execute(line);
+		}
+
+		fclose(file);
+	}
+	else
+	{
+		printf("Usage: %s [script_file]\n", argv[0]);
+		return (1);
+	}
+
+	return (0);
+}
