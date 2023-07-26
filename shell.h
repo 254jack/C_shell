7a6677ff93899aa62758f705748cf20df1c9342c
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-07-26 23:37:05 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,22 +1,100 @@
-#ifndef SHELL_H
-#define SHELL_H
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <string.h>
 #include <sys/wait.h>
 
-#define EXIT_SHELL 10
-#define ARG_MAX 64
+#define BUFFER_SIZE 1024
 
-int main(void);
-char *read_line(void);
-int execute_command(char *command);
-int execute_cd(char **args);
-int execute_env(void);
-int execute_external(char **args);
-void print_prompt(void);
+/**
+ * display_prompt - Displays the shell prompt.
+ */
+void display_prompt(void);
 
-#endif /* SHELL_H */
+/**
+ * main - Entry point of the simple shell.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char buffer[BUFFER_SIZE];
+    ssize_t read_bytes;
+    int exit_shell = 0;
+    pid_t pid; /* Move the declaration of pid outside of the block */
+
+    while (!exit_shell)
+    {
+        display_prompt();
+
+        read_bytes = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+        if (read_bytes == -1)
+        {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+
+        /* Handle end of file (Ctrl+D) */
+        if (read_bytes == 0)
+        {
+            putchar('\n');
+            exit_shell = 1;
+            continue;
+        }
+
+        /* Null-terminate the input string */
+        buffer[read_bytes - 1] = '\0';
+
+        /* Check if the user entered 'exit' command */
+        if (strcmp(buffer, "exit") == 0)
+        {
+            exit_shell = 1;
+            continue;
+        }
+
+        /* Fork to create a child process */
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0)
+        {
+            /* In the child process */
+            char *argv[2];
+            char *path = strchr(buffer, '/');
+            if (path)
+                argv[0] = path; /* Use the full path */
+            else
+                argv[0] = buffer; /* Search in the PATH */
+
+            argv[1] = NULL;
+
+            /* Execute the command using execve */
+            if (execve(argv[0], argv, NULL) == -1)
+            {
+                perror(argv[0]);
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            /* In the parent process */
+            /* Wait for the child process to complete */
+            int status;
+            waitpid(pid, &status, 0);
+        }
+    }
+
+    return 0;
+}
+
+/**
+ * display_prompt - Displays the shell prompt.
+ */
+void display_prompt(void)
+{
+    printf("#cisfun$ ");
+}
