f40d559678abccaaf34f1ea6d9ec03a085efed23
254jack <taixonj@gmail.com>
2023-07-26 20:59:01 +0300

    betty.c

--- betty.c
+++ betty.c
@@ -1,9 +1,12 @@
 #include "shell.h"
+
 /**
  * betty - betty function
  * Return: 0
-*/
-int betty(void){
-  printf("Hello, Jack && Chacha");
-  return (0);
+ */
+int betty(void)
+{
+    printf("Hello, Jack && Chacha\n");
+
+    return (0);
 }
