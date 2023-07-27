7320228c8efd6de2ef3b83e9b9f7c85e9bb6d0c3
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-07-28 00:44:06 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,6 +1,5 @@
 #ifndef SHELL_H
 #define SHELL_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,8 +7,11 @@
 #include <sys/wait.h>
 
 #define MAX_ARGS 10
+#define _GNU_SOURCE
+
+void prompt(char **av, char **env);
 void lid_ln(char *str);
 void tokenizeCmd(char *cmd, char **argv);
 void executeCmd(char *cmd, char **argv, char **env);
-void prompt(char **av, char **env);
-#endif
+
+#endif /* SHELL_H */
