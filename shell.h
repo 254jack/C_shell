bc14d98b96807d188567e661bf690e78f602b285
raptorcastz <123360400+raptorcastz@users.noreply.github.com>
2023-07-27 05:43:49 +0300

    shell.h

--- shell.h
+++ shell.h
@@ -8,6 +8,16 @@
 #include <sys/wait.h>
 
 #define MAX_ARGS 10
+/**
+  *removes the new line charatcter from the string.
+  *@param str the string to modify
+**/
 
 void prompt(char **av, char **env);
+/**
+  tokenize cmd string to tokenize arguments
+  *@param cmd The command string to tokenize
+ * @param argv The
+ * Description: array to store the arguments
+**/
 #endif
