232305055e1aba06f2eeb1980491a0e1314a77e5
raptorcastz <chachaamaziah@gmail.com>
2023-07-28 22:38:33 +0200

    shell.h

--- shell.h
+++ shell.h
@@ -3,9 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <limits.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 
-#define MAX_ARGS 10git
-#define _GNU_SOURCE
+#define MAX_ARGS 10
+#define _GNU_SOURCEls
