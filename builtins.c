33a7e949a4cff394f854f0be7fb5bbab6ceb33a5
254jack <taixonj@gmail.com>
2023-07-28 15:27:06 +0300

    add

--- builtins.c
+++ builtins.c
@@ -2,24 +2,13 @@
 /**
  * h_exit - a function that handles exit command
  * @cmd: command
-*/
+ */
 void h_exit(char *cmd)
 {
 	if (strcmp(cmd, "exit") == 0)
 	{
-		char *stat = strtok(NULL, " ");
 
-		if (stat != NULL)
-		{
-			int status = atoi(stat);
-
-			free(cmd);
-			exit(status);
-		}
-		else
-		{
-			free(cmd);
-			exit(EXIT_SUCCESS);
-		}
+		free(cmd);
+		exit(EXIT_SUCCESS);
 	}
 }
