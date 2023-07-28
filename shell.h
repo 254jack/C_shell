162450563fb1674fe8403cb4c7bafc3cfcff6fb4
raptorcastz <chachaamaziah@gmail.com>
2023-07-28 22:34:12 +0200

    shell.h

--- shell.h
+++ shell.h
@@ -3,17 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <limits.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
-#define MAX_ARGS 10
+#define MAX_ARGS 10git
 #define _GNU_SOURCE
-
-void prompt(char **av, char **env);
-void lid_ln(char *str);
-void tokenizeCmd(char *cmd, char **argv);
-void executeCmd(char *cmd, char **argv, char **env);
-void h_exit(char *cmd);
-
-#endif
-
